c2cb73fb18beacc52300f20cfbe1171afecbe287
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-02-02 16:32:26 -0500

    Implement pin controller create specs

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,2 +1,6 @@
+
 class Pin < ApplicationRecord
+  validates :item_name, length: { minimum: 3, maximum: 256 }
+  validates :description, length: { minimum: 12, maximum: 256 }
+  validates :transaction_type, presence: true
 end
--- spec/controllers/pins_controller_spec.rb
+++ spec/controllers/pins_controller_spec.rb
@@ -2,31 +2,51 @@ require 'rails_helper'
 
 RSpec.describe PinsController do
 
-  describe 'get /pins.json' do
+  describe 'GET /pins.json' do
     context 'successful response' do
-      before(:all){ create_list(:pin, 10) }
+      before(:each){ create_list(:pin, 10) }
       let( :json ){ JSON.parse( response.body ) }
+
+      it 'the correct number of items is returned' do
+        get :index, format: :json
+        expect(json.length).to eq(10)
+      end
 
       it 'returns a valid JSON object' do
         get :index, format: :json
-        expect(json).to be_a Array 
+        expect(json).to be_a Array
       end
 
       it 'the array contains hashes' do
         get :index, format: :json
-        expect(json[0]).to be_a Hash 
-      end
-
-      it 'the correct number of items is returned' do
-        get :index, format: :json
-        expect(json.length).to eq(10) 
+        expect(json[0]).to be_a Hash
       end
 
     end
   end
 
-   describe 'create /pins.json' do
+  describe 'POST /pins.json' do
+    context 'valid data' do
+      it 'creates a new pin' do
+        valid_pin_data = { item_name: 'cheese',
+                           description: 'the best thing ever',
+                           transaction_type: 'buy' }
+        expect {
+          post :create, pin: valid_pin_data
+        }.to change { Pin.count }.by(1)
+      end
+    end
 
-   end
+    context 'invalid data' do
+      it 'does not create a new pin' do
+        invalid_pin_data = { item_name: 'x',
+                             description: 'too short',
+                             transaction_type: 'buy' }
+        expect {
+          post :create, pin: invalid_pin_data
+        }.to change { Pin.count }.by(0)
+      end
+    end
+  end
 
 end
